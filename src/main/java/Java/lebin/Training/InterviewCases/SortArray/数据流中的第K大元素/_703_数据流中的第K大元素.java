0d41f0fdce11857d3792acf17905948b726e93d1
liulebin <liulebin@58.com>
2024-03-16 21:23:19 +0800

    回文串二刷

--- src/main/java/Java/lebin/Training/InterviewCases/SortArray/数据流中的第K大元素/_703_数据流中的第K大元素.java
+++ src/main/java/Java/lebin/Training/InterviewCases/SortArray/数据流中的第K大元素/_703_数据流中的第K大元素.java
@@ -19,7 +19,12 @@ int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 
  */
 public class _703_数据流中的第K大元素 {
 }
-
+/*
+方法一：优先队列
+我们可以使用一个大小为 k 的优先队列来存储前 k 大的元素，其中优先队列的队头为队列中最小的元素，也就是第 k 大的元素。
+在单次插入的操作中，我们首先将元素 val 加入到优先队列中。如果此时优先队列的大小大于 k，我们需要将优先队列的队头元素弹出，
+以保证优先队列的大小为 k。
+ */
 class KthLargest {
     private PriorityQueue<Integer> minHeap;
     private int k;
