08c89e93c6cc8795b3a0e8d3b8ddb8d9598a94cd
liulebin <liulebin@58.com>
2024-03-17 18:11:06 +0800

    回文串二刷

---
+++ src/main/java/Java/lebin/Training/InterviewCases/矩阵/_62_不同路径_左上角到右下角多少不同的路径.java
@@ -0,0 +1,35 @@
+package Java.lebin.Training.InterviewCases.矩阵;
+/*
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+问总共有多少条不同的路径？
+ */
+//时间复杂度：O(mn)
+//空间复杂度：O(mn)
+public class _62_不同路径_左上角到右下角多少不同的路径 {
+    /**
+     * 1. 确定dp数组下标含义 dp[i][j] 到每一个坐标可能的路径种类
+     * 2. 递推公式 dp[i][j] = dp[i-1][j]+dp[i][j-1]
+     * 3. 初始化 dp[i][0]=1 dp[0][i]=1 初始化横竖都可
+     * 4. 遍历顺序 一行一行遍历
+     * 5. 推导结果 。。。。。。。。
+     */
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        //初始化
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
